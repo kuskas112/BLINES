265e3563ebef5636659de61e39488674fd69b695
kuskas112 <vasileniyaivanka@mail.ru>
2026-02-14 13:08:18 +0300

    Update SpellButtonSpawner.cs

--- Assets/Scripts/UI/Button/SpellButton/SpellButtonSpawner.cs
+++ Assets/Scripts/UI/Button/SpellButton/SpellButtonSpawner.cs
@@ -5,7 +5,7 @@ public class SpellButtonSpawner : ButtonSpawner<SpellButtonFacade>
     public override SpellButtonFacade Spawn(Vector3 position, Quaternion rotation)
     {
         SpellButtonFacade facade = base.Spawn(position, rotation);
-        facade.spellButton.MoveObjectInsideButton();
+        facade.behaviour.MoveObjectInsideButton();
         return facade;
     }
 }
