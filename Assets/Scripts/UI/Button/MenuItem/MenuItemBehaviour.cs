8650ed6edb18175903b6af0d269adb7ea36a3377
kuskas112 <vasileniyaivanka@mail.ru>
2026-02-25 13:50:01 +0300

    animation for menu item

--- Assets/Scripts/UI/Button/MenuItem/MenuItemBehaviour.cs
+++ Assets/Scripts/UI/Button/MenuItem/MenuItemBehaviour.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class MenuItemBehaviour : ButtonBehaviour
@@ -8,16 +9,18 @@ public class MenuItemBehaviour : ButtonBehaviour
         base.Awake();
         facade = GetComponent<ButtonFacade>();
     }
-
-    protected override void PlayDefaultAnimation()
-    {
-        facade.animator.StartGlowToggleAnimation();
-        isClicked = false;
-    }
     
     protected override void PlaySpecificAnimation()
     {
+        isClicked = false;
+        StartCoroutine(GlowAndMoveButton(new(0f, -7f)));
+    }
 
+    IEnumerator GlowAndMoveButton(Vector2 targetPos) 
+    {
+        facade.animator.StartGlowToggleAnimation();
+        yield return new WaitForSeconds(facade.animator.duration);
+        facade.mover.MoveEaseOut(targetPos, 1.5f);
     }
 
 }
